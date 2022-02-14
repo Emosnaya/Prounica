1964db675f215ec482e09fefa8cf50f0da377450
Emosnaya <emmanuel.osnaya@ingenieria.unam.edu>
2022-02-14 10:59:32 -0600

    Se agregó las 9 clases

---
+++ classPokemons18.py
@@ -0,0 +1,45 @@
+from classPokemon import Pokemon
+
+class Fire(Pokemon):
+    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad):
+        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad)
+
+class Electric(Pokemon):
+    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad):
+        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad)
+    
+
+class Fly(Pokemon):
+    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad):
+        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad)
+
+
+
+class Ice(Pokemon):
+    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad):
+        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad)
+
+
+class Rock(Pokemon):
+    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad):
+        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad)
+
+
+class Ground(Pokemon):
+    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad):
+        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad)
+
+
+class Steel(Pokemon):
+    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad):
+        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad)
+
+
+class Normal(Pokemon):
+    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad):
+        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad)
+
+
+class Bug(Pokemon):
+    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad):
+        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad)
