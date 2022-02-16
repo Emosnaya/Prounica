83ae53e40cc47fab27640e17e070121bbbc416cd
R2D2Alfa <83356030+R2D2Alfa@users.noreply.github.com>
2022-02-16 12:49:37 -0600

    Update ClassPokemon10-18.py
    Classes 10-18

---
+++ ClassPokemon10-18.py
@@ -0,0 +1,46 @@
+from classPokemon import Pokemon
+
+class Water(Pokemon):
+    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad):
+        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad)
+
+
+class Plant(Pokemon):
+    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad):
+        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad)
+    
+
+
+class Poison(Pokemon):
+    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad):
+        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad)
+
+
+class Fairy(Pokemon):
+    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad):
+        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad)
+
+
+class Dragon(Pokemon):
+    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad):
+        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad)
+
+
+class Dark(Pokemon):
+    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad):
+        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad)
+
+
+class Fighter(Pokemon):
+    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad):
+        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad)
+
+
+class Ghost(Pokemon):
+    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad):
+        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad)
+
+
+class Psychic(Pokemon):
+    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad):
+        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad)
