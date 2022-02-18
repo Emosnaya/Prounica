216bb10c5b6c4876f0b0e142808678fb68578234
R2D2Alfa <83356030+R2D2Alfa@users.noreply.github.com>
2022-02-17 22:06:39 -0600

    Cambios atributos
    Se cambiaron todas las clases con los atributos sintetizados

--- ClassPokemon10-18.py
+++ ClassPokemon10-18.py
@@ -1,46 +1,91 @@
 from classPokemon import Pokemon
 
 class Water(Pokemon):
-    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad):
-        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad)
+    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, hp, ataque, ataquesp,):
+        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria)
+        self.habilidad = 'Bueno contra Hielo'
+        self.debilida = 'Débil contra hielo'
+        self.hp = hp
+        self.ataque = ataque
+        self.ataquesp = ataquesp
 
 
 class Plant(Pokemon):
-    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad):
-        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad)
-    
+    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, hp, ataque, ataquesp,):
+        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria)
+        self.habilidad = 'Bueno contra Hielo'
+        self.debilida = 'Débil contra hielo'
+        self.hp = hp
+        self.ataque = ataque
+        self.ataquesp = ataquesp
+
 
 
 class Poison(Pokemon):
-    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad):
-        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad)
+    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, hp, ataque, ataquesp,):
+        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria)
+        self.habilidad = 'Bueno contra Hielo'
+        self.debilida = 'Débil contra hielo'
+        self.hp = hp
+        self.ataque = ataque
+        self.ataquesp = ataquesp
 
 
 class Fairy(Pokemon):
-    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad):
-        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad)
+    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, hp, ataque, ataquesp,):
+        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria)
+        self.habilidad = 'Bueno contra Hielo'
+        self.debilida = 'Débil contra hielo'
+        self.hp = hp
+        self.ataque = ataque
+        self.ataquesp = ataquesp
 
 
 class Dragon(Pokemon):
-    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad):
-        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad)
+    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, hp, ataque, ataquesp,):
+        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria)
+        self.habilidad = 'Bueno contra Hielo'
+        self.debilida = 'Débil contra hielo'
+        self.hp = hp
+        self.ataque = ataque
+        self.ataquesp = ataquesp
 
 
 class Dark(Pokemon):
-    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad):
-        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad)
+    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, hp, ataque, ataquesp,):
+        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria)
+        self.habilidad = 'Bueno contra Hielo'
+        self.debilida = 'Débil contra hielo'
+        self.hp = hp
+        self.ataque = ataque
+        self.ataquesp = ataquesp
 
 
 class Fighter(Pokemon):
-    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad):
+    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, hp, ataque, ataquesp,):
         super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad)
+        self.habilidad = 'Bueno contra Hielo'
+        self.debilida = 'Débil contra hielo'
+        self.hp = hp
+        self.ataque = ataque
+        self.ataquesp = ataquesp
 
 
 class Ghost(Pokemon):
-    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad):
+    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, hp, ataque, ataquesp,):
         super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad)
+        self.habilidad = 'Bueno contra Hielo'
+        self.debilida = 'Débil contra hielo'
+        self.hp = hp
+        self.ataque = ataque
+        self.ataquesp = ataquesp
 
 
 class Psychic(Pokemon):
-    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad):
+    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, hp, ataque, ataquesp,):
         super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad)
+        self.habilidad = 'Bueno contra Hielo'
+        self.debilida = 'Débil contra hielo'
+        self.hp = hp
+        self.ataque = ataque
+        self.ataquesp = ataquesp
--- classPokemons18.py
+++ classPokemons18.py
@@ -10,36 +10,73 @@ class Fire(Pokemon):
         self.ataquesp = ataquesp
 
 class Electric(Pokemon):
-    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad):
-        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad)
-    
+    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, hp, ataque, ataquesp,):
+        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria)
+        self.habilidad = 'Bueno contra Hielo'
+        self.debilida = 'Débil contra hielo'
+        self.hp = hp
+        self.ataque = ataque
+        self.ataquesp = ataquesp
 
 class Fly(Pokemon):
-    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad):
-        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad)
-
+    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, hp, ataque, ataquesp,):
+        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria)
+        self.habilidad = 'Bueno contra Hielo'
+        self.debilida = 'Débil contra hielo'
+        self.hp = hp
+        self.ataque = ataque
+        self.ataquesp = ataquesp
 
 class Ice(Pokemon):
-    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad):
-        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad)
+    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, hp, ataque, ataquesp,):
+        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria)
+        self.habilidad = 'Bueno contra Hielo'
+        self.debilida = 'Débil contra hielo'
+        self.hp = hp
+        self.ataque = ataque
+        self.ataquesp = ataquesp
 
 class Rock(Pokemon):
-    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad):
-        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad)
-
+    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, hp, ataque, ataquesp,):
+        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria)
+        self.habilidad = 'Bueno contra Hielo'
+        self.debilida = 'Débil contra hielo'
+        self.hp = hp
+        self.ataque = ataque
+        self.ataquesp = ataquesp
 
 class Ground(Pokemon):
-    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad):
-        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad)
+    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, hp, ataque, ataquesp,):
+        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria)
+        self.habilidad = 'Bueno contra Hielo'
+        self.debilida = 'Débil contra hielo'
+        self.hp = hp
+        self.ataque = ataque
+        self.ataquesp = ataquesp
 
 class Steel(Pokemon):
-    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad):
-        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad)
+    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, hp, ataque, ataquesp,):
+        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria)
+        self.habilidad = 'Bueno contra Hielo'
+        self.debilida = 'Débil contra hielo'
+        self.hp = hp
+        self.ataque = ataque
+        self.ataquesp = ataquesp
 
 class Normal(Pokemon):
-    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad):
-        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad)
+    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, hp, ataque, ataquesp,):
+        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria)
+        self.habilidad = 'Bueno contra Hielo'
+        self.debilida = 'Débil contra hielo'
+        self.hp = hp
+        self.ataque = ataque
+        self.ataquesp = ataquesp
 
 class Bug(Pokemon):
-    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad):
-        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad)
+    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, hp, ataque, ataquesp,):
+        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria)
+        self.habilidad = 'Bueno contra Hielo'
+        self.debilida = 'Débil contra hielo'
+        self.hp = hp
+        self.ataque = ataque
+        self.ataquesp = ataquesp
