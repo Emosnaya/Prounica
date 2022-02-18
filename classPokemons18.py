3f7dcd42f7abc28fff622f0383bd9fc7f5ca6a0c
Emosnaya <emmanuel.osnaya@ingenieria.unam.edu>
2022-02-17 22:48:12 -0600

    Se agregó las funciones de aatque

--- classPokemons18.py
+++ classPokemons18.py
@@ -1,3 +1,4 @@
+from random import randint
 from classPokemon import Pokemon
 
 class Fire(Pokemon):
@@ -8,6 +9,32 @@ class Fire(Pokemon):
         self.hp = hp
         self.ataque = ataque
         self.ataquesp = ataquesp
+    
+    def inflingeDaño(self):
+        return randint(1, self.ataque)
+    
+    def inflingeDaño(self, modificador ='Normal'):
+        daño = 0
+        if (modificador == 'ventaja'):
+            daño = randint(1, self.ataquesp)+randint(1,10)
+        elif (modificador == 'desventaja'):
+            while daño <= 0:
+                daño = randint(1,self.ataque)-randint(1,self.ataque-3)
+        else: 
+            daño = randint(1, self.ataque)
+        
+        return daño
+    
+    def recibeAtaque(self,daño):
+        self.hp-daño
+    
+
+
+    
+
+
+    
+
 
 class Electric(Pokemon):
     def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, hp, ataque, ataquesp):
