767afc88d6baf005192b77662e0ccb6ced8b4237
Emosnaya <emmanuel.osnaya@ingenieria.unam.edu>
2022-02-17 21:06:34 -0600

    Se modificó la clase fuego

--- classPokemons18.py
+++ classPokemons18.py
@@ -1,8 +1,13 @@
 from classPokemon import Pokemon
 
 class Fire(Pokemon):
-    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad):
-        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad)
+    def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, hp, ataque, ataquesp,):
+        super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria)
+        self.habilidad = 'Bueno contra Hielo'
+        self.debilida = 'Débil contra hielo'
+        self.hp = hp
+        self.ataque = ataque
+        self.ataquesp = ataquesp
 
 class Electric(Pokemon):
     def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad):
@@ -12,7 +17,6 @@ class Electric(Pokemon):
 class Fly(Pokemon):
     def __init__(self, nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad):
         super().__init__(nombre, numero, tipo, altura, peso, sexo, categoria, habilidad, ps, ataque, defensa, ataqueEsp, defensaEsp, velocidad, debilidad)
-
 
 
 class Ice(Pokemon):
